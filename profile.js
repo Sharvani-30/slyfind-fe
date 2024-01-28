8a577be0347b1e4253886dd05db157b773ca3d95
Sharvani-30 <sharvanikomuravelly2004@gmail.com>
2024-01-28 23:15:54 +0530

    change

--- profile.js
+++
@@ -1,6 +0,0 @@
- document.querySelectorAll('.profile').forEach((profile) => {
-        profile.addEventListener('click', () => {
-            profile.classList.toggle('expanded');
-        });
-    });
-    
